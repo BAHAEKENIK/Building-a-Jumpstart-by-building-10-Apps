65529b5584ab34b4b2ac17ffc09e9b9aabf43821
bahae kenikssi <bahaekenik@gmail.com>
2025-07-30 10:56:35 +0100

    App 2

--- App 2 Guess that number game/programe.py
+++ App 2 Guess that number game/programe.py
@@ -1,13 +1,29 @@
 from random import randint
+
+
 def main():
-    pass
+    header()
+    player, computer = get_numbers()
+    while player != computer:
+        if player < computer:
+            print(f"Sorry but {player} is LOWER than the number")
+        else:
+            print(f"Sorry but {player} is HIGHER than the number")
+        player = int(input("Guess a number between 0 and 100:"))
+    print(f"YES! You've got it. The number was {computer}")
+
+
 def header():
-    print('----------------------------------------------------')
-    print('               GUESS THE NUMBER APP                 ')
-    print('----------------------------------------------------')
+    print("----------------------------------------------------")
+    print("               GUESS THE NUMBER APP                 ")
+    print("----------------------------------------------------")
+
 
 def get_numbers():
-    request=int(input('Guess a number between 0 and 10 '))
+    request = int(input("Guess a number between 0 and 100:"))
+    computer = randint(1, 100)
+    return request, computer
+
 
 if __name__ == "__main__":
     main()
